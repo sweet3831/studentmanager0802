2341ef086ec41ffdb943481f5e96fd2f705ea887
sweet3831 <april42817@naver.com>
2017-08-02 17:42:37 +0900

    Add files via upload

---
+++ StudentScoreAddServie.java
@@ -0,0 +1,13 @@
+package kosta.student.service;
+
+import java.util.Scanner;
+
+public class StudentScoreAddServie implements StudentService {
+
+	@Override
+	public void execute(Scanner scan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
