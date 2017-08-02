3a130026fbe5ddd9968e081ec1d1abf999398b17
sweet3831 <april42817@naver.com>
2017-08-02 17:44:22 +0900

    Add files via upload

---
+++ StudentService.java
@@ -0,0 +1,16 @@
+package kosta.student.service;
+
+import java.util.Scanner;
+
+//추상메소드 밖에 만들지 못한다. 
+//여기는 인터페이스 코딩 노노노
+public interface StudentService {
+
+	void execute(Scanner scan);
+	
+}
+
+
+
+
+
